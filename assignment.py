bb7599a0b7c12f2def04948e812e1246e2d93b95
Issac <tsuncp@gmail.com>
2020-03-27 15:29:08 +0800

    add Providing where can buy surgical mask add Providing Personal hygienic advice add Providing Symptoms of coronavirus add Providing useful contact numbers add Record and show temperature record function

--- assignment.py
+++ assignment.py
@@ -5,6 +5,8 @@ from io import StringIO
 import pandas as pd
 import requests
 import redis
+import pymongo
+from datetime import datetime
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -21,7 +23,11 @@ line_bot_api = LineBotApi(key.key)
 handler = WebhookHandler(key.secret)
 
 r = redis.Redis(host='localhost', port=6379, db=0)
-      
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["hkbupj"]
+temperatureHistoryDb = mydb["temperatureHistory"]
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -104,7 +110,60 @@ def handle_message(event):
             line_bot_api.reply_message(
                 event.reply_token, help_message
             )
-
+        elif event.message.text.startswith("@contact"):
+            result = r.get("contact").decode('utf-8')        
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=result)
+            )
+        elif event.message.text.startswith("@symptoms"):
+            result = r.get("symptoms").decode('utf-8')        
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=result)
+            )
+        elif event.message.text.startswith("@advice"):
+            result = r.get("advice").decode('utf-8')        
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=result)
+            )
+        elif event.message.text.startswith("@whereBuySurgicalMask"):
+            result = r.get("mask").decode('utf-8')        
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=result)
+            )
+        elif event.message.text.startswith("@showRecords"):
+            data = event.message.text.split(" ")
+            historys = []
+            result = ""
+            orderString = "Descending"
+            length = 10
+            order = -1
+            if len(data) == 3:
+                try:
+                    if data[1] == "asc":
+                        orderString = "Ascending"
+                        order = 1;
+                    else:
+                        orderString = "Descending"
+                        order = -1;
+                    length = int(data[2])
+                except ValueError:
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text="Invalid Input.\nTo show more, use @showRecords Order Limit,\nFor Example : @showRecords asc 10")
+                    )
+                    return
+            historys = temperatureHistoryDb.find({"user": event.source.user_id}).sort("time", order).limit(length)
+            for x in historys:
+                result += str(x["temperature"]) +" At "+x["time"].strftime("%Y-%m-%d %H:%M:%S") +"\n"
+            last = r.get('lastTemp-'+event.source.user_id).decode('utf-8')
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="Last Temperature: "+str(last)+"\nShowing Last "+str(length)+" Records In "+orderString+" Order : \n\n"+result+"\nTo show more, use @showRecords Order Limit,\nFor Example : @showRecords asc 10")
+            )
         elif event.message.text.startswith("@record"):
             data = event.message.text.split(" ")
             if len(data) != 2:
@@ -116,6 +175,8 @@ def handle_message(event):
                 try:
                     temp = float(data[1])
                     r.set('lastTemp-'+event.source.user_id, temp)
+                    record = { "user": event.source.user_id, "temperature": temp, "time":datetime.now() }
+                    temperatureHistoryDb.insert_one(record) 
                     if temp > 37.5:
                         line_bot_api.reply_message(
                             event.reply_token,
@@ -142,7 +203,7 @@ def handle_message(event):
             cases = c.groupby("Case classification*")
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text="Latest Status : \n\nTotal Cases : "+str(len(c))+"\nMale : "+str(len(genders.get_group("M")))+" Female : "+str(len(genders.get_group("F")))+"\nHospitalised : "+str(len(status.get_group("Hospitalised")))+"\nDischarge : "+str(len(status.get_group("Discharged")))+"\nDeceased : "+str(len(status.get_group("Deceased")))+"\nHK resident : "+str(len(residents.get_group("HK resident")))+"\nNon-HK resident : "+str(len(residents.get_group("Non-HK resident")))+"\nImported : "+str(len(cases.get_group("Imported")))+"\nClose contact of imported case : "+str(len(cases.get_group("Close contact of imported case")))+"\nPossibly local : "+str(len(cases.get_group("Possibly local")))+"\nClose contact of possibly local case : "+str(len(cases.get_group("Close contact of possibly local case")))+"")
+                TextSendMessage(text="Latest Status : \n\nTotal Cases : "+str(len(c))+"\nMale : "+str(len(genders.get_group("M")))+" Female : "+str(len(genders.get_group("F")))+"\nHospitalised : "+str(len(status.get_group("Hospitalised")))+"\nDischarge : "+str(len(status.get_group("Discharged")))+"\nDeceased : "+str(len(status.get_group("Deceased")))+"\nHK resident : "+str(len(residents.get_group("HK resident")))+"\nNon-HK resident : "+str(len(residents.get_group("Non-HK resident")))+"\nImported : "+str(len(cases.get_group("Imported")))+"\nClose contact of imported case : "+str(len(cases.get_group("Close contact of imported case")))+"\nPossibly local : "+str(len(cases.get_group("Possibly local")))+"\nClose contact of possibly local case : "+str(len(cases.get_group("Close contact of possibly local case")))+"\n\nFor more detail, please access https://chp-dashboard.geodata.gov.hk/covid-19/en.html")
             )
 
         else:
